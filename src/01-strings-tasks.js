fc437d17d20178f7aafffa29877e3dfe866f0821
ValentinSinyakin <valentinbrest95@gmail.com>
2022-07-25 22:15:23 +0300

    fix: string tasks 16

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -281,8 +281,18 @@ function isString(value) {
  *   'Q♠' => 50
  *   'K♠' => 51
  */
-function getCardId(/* value */) {
-  throw new Error('Not implemented');
+function getCardId(value) {
+  const cards = ['A♣', '2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', '10♣', 'J♣', 'Q♣', 'K♣',
+    'A♦', '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', '10♦', 'J♦', 'Q♦', 'K♦',
+    'A♥', '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', '10♥', 'J♥', 'Q♥', 'K♥',
+    'A♠', '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', '10♠', 'J♠', 'Q♠', 'K♠'];
+  let res = '';
+  for (let i = 0; i < cards.length; i += 1) {
+    if (value === cards[i]) {
+      res += i;
+    }
+  }
+  return res;
 }
 
 
